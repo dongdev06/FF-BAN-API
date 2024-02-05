fb659e4b7c80175625316ad57d07f40217d7f5c3
TheekshanaPathirane <thiwankapathirane293@gmail.com>
2024-02-05 18:53:39 +0530

    FIRST

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route('/ban_api/<uid>', methods=['GET'])
+def check_banned(uid):
+    url = f"https://ff.garena.com/api/antihack/check_banned?lang=en&uid={uid}"
+
+    headers = {
+        'User-Agent': "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36",
+        'Accept': "application/json, text/plain, /",
+        'authority': "ff.garena.com",
+        'accept-language': "en-GB,en-US;q=0.9,en;q=0.8",
+        'referer': "https://ff.garena.com/en/support/",
+        'sec-ch-ua': "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\"",
+        'sec-ch-ua-mobile': "?1",
+        'sec-ch-ua-platform': "\"Android\"",
+        'sec-fetch-dest': "empty",
+        'sec-fetch-mode': "cors",
+        'sec-fetch-site': "same-origin",
+        'x-requested-with': "B6FksShzIgjfrYImLpTsadjS86sddhFH",
+        'Cookie': "Your cookies here"
+    }
+
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # Check for HTTP errors
+
+        return jsonify(response.json())
+
+    except requests.exceptions.RequestException as e:
+        return jsonify({'error': str(e)}), 500  # Return a 500 Internal Server Error on request failure
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
